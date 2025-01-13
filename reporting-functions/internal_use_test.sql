e8f260b9108e410c31de81c3fa1cb6c69e1771e9
lrickards <lrickards@scu.edu>
2025-01-13 15:07:05 -0800

    Update internal_use_test.sql

--- reporting-functions/internal_use_test.sql
+++ reporting-functions/internal_use_test.sql
@@ -4,9 +4,9 @@ DROP FUNCTION IF EXISTS internal_use_test;
 	
 CREATE FUNCTION internal_use_test(barcode bigint)
 returns table(
-	cit.item_id integer,
-	cit.item_statu_prior_to_check_in,
-	itt.barcode
+	cit.item_id uuid,
+	cit.item_statu_prior_to_check_in text,
+	itt.barcode bigint
 )
 as $$
 select cit.item_id, cit.item_status_prior_to_check_in, itt.barcode from folio_circulation.check_in__t__ as cit JOIN folio_inventory.item__t__ as itt ON cit.item_id = itt.id
