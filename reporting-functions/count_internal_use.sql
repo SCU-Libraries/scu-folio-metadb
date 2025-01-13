f159522f2ef1a0c2044fc55da79ce0b5cce82499
lrickards <lrickards@scu.edu>
2025-01-13 14:28:51 -0800

    Update count_internal_use.sql

--- reporting-functions/count_internal_use.sql
+++ reporting-functions/count_internal_use.sql
@@ -1,18 +1,17 @@
 --metadb:function count_internal_use
 
-DROP FUNCTION IF EXISTS count_internal_use
+DROP FUNCTION IF EXISTS count_internal_use;
 	
-CREATE FUNCTION count_internal_use(
-    barcode bigint)
+CREATE FUNCTION count_internal_use(barcode bigint)
 returns table(
 	internal_use_count integer)
 as $$
 select count(cit.id) AS internal_use_count
-	from folio_circulation.check_in__t__ cit JOIN folio_inventory.item__t__ itt ON cit.item_id = itt.id
-	WHERE
-		itt.__current = true and
-		itt.barcode = barcode and
-		cit.item_status_prior_to_check_in = 'Available'
+  from folio_circulation.check_in__t__ as cit JOIN folio_inventory.item__t__ as itt ON cit.item_id = itt.id
+  WHERE
+    itt.__current = true and
+    itt.barcode = barcode and
+    cit.item_status_prior_to_check_in = 'Available'
 $$
 language sql
 STABLE 
