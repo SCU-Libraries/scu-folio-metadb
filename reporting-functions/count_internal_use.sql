a0fd9af85af25f537b4971d6bc0e9fb362059845
lrickards <lrickards@scu.edu>
2025-01-14 09:54:20 -0800

    Update count_internal_use.sql

--- reporting-functions/count_internal_use.sql
+++ reporting-functions/count_internal_use.sql
@@ -3,7 +3,7 @@
 DROP FUNCTION IF EXISTS count_internal_use;
 	
 CREATE FUNCTION count_internal_use(
-	barcode text
+	barcode_to_check text
 )
 returns table(
 	internal_use_count integer
@@ -13,7 +13,7 @@ select count(cit.id) AS internal_use_count
   from folio_circulation.check_in__t__ as cit JOIN folio_inventory.item__t__ as itt ON cit.item_id = itt.id
   WHERE
     itt.__current = true and
-    itt.barcode = barcode and
+    itt.barcode = barcode_to_check and
     cit.item_status_prior_to_check_in = 'Available'
 $$
 language sql
