25b07f13fbd41b156409bb6ea911507dc6e2c9c6
lrickards <lrickards@scu.edu>
2025-01-24 13:51:02 -0800

    Update class_list.sql

--- reporting-functions/class_list.sql
+++ reporting-functions/class_list.sql
@@ -29,6 +29,18 @@ FROM
     itt.barcode = barcode_to_check and
     cit.item_status_prior_to_check_in = 'Checked out' and
     start_date <= cit.occurred_date_time and cit.occurred_date_time <= end_date
+
+	SELECT item.id AS item_id,
+       jsonb_extract_path_text(notes.jsonb, 'id')::uuid AS note_id,
+       jsonb_extract_path_text(notes.jsonb, 'note') AS note,
+       jsonb_extract_path_text(notes.jsonb, 'noteType') AS note_type,
+       jsonb_extract_path_text(notes.jsonb, 'staffOnly')::boolean AS staff_only,
+       notes.ordinality
+FROM folio_inventory.item
+    CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(jsonb, 'circulationNotes'))
+        WITH ORDINALITY AS notes (jsonb);
+
+
 $$
 language sql
 STABLE 
