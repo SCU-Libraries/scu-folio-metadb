9ccb2c65d9ab460d7261090f1781eb05ae4168c8
lrickards <lrickards@scu.edu>
2025-01-24 10:28:57 -0800

    Update class_list.sql

--- reporting-functions/class_list.sql
+++ reporting-functions/class_list.sql
@@ -1,1 +1,25 @@
+--metadb:function class_list
 
+DROP FUNCTION IF EXISTS class_list;
+	
+CREATE FUNCTION class_list(
+	barcode_to_check text,
+	start_date date DEFAULT '2000-01-01',
+  	end_date date DEFAULT '2099-01-01'
+)
+returns table(
+	Check_In_ID text,
+	Check_In_Date date
+)
+as $$
+select cit.id AS Check_In_ID, cit.occurred_date_time as Check_In_Date
+  from folio_circulation.check_in__t__ as cit JOIN folio_inventory.item__t__ as itt ON cit.item_id = itt.id
+  WHERE
+    itt.__current = true and
+    itt.barcode = barcode_to_check and
+    cit.item_status_prior_to_check_in = 'Checked out' and
+    start_date <= cit.occurred_date_time and cit.occurred_date_time <= end_date
+$$
+language sql
+STABLE 
+PARALLEL SAFE; 
