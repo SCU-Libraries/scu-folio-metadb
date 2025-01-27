7690cf2fe1576ffa6f670d9050f918b0c3091816
lrickards <lrickards@scu.edu>
2025-01-27 12:10:26 -0800

    Update class_list.sql

--- reporting-functions/class_list.sql
+++ reporting-functions/class_list.sql
@@ -6,38 +6,31 @@ CREATE FUNCTION class_list(
 	note_string text
 )
 returns table(
-	item_shelving_location test,
+	item_barcode,
+	item_shelving_location_code text,
+	item_shelving_location_name text,
 	call_number text,
-	item_volume_number text,
-	instance_author text,
 	instance_title text,
-	instance_publication_info text
+	item_note text
 )
 as $$
-select i.effective_location_id AS item_shelving_location, i.effective_shelving_order as call_number, i.volume as item_volume_number, instance.author as instance_author, instance.title as instnace_title, instance.publication_info as instance_publication_info
-
-FROM folio_inventory.item__ as i
-CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(i.jsonb, 'notes')) WITH ORDINALITY AS notes (data)
-    LEFT JOIN folio_inventory.holdings__t ON instance__t.SOMETHING = holdings__t.SOMETHING
-    LEFT JOIN folio_inventory.item__t ON holdings__t.SOMETHING = item__t.SOMETHING
 	
-	WHERE
-	notes.content LIKE '%' + note_string + '%'
-    itt.__current = true and
-    itt.barcode = barcode_to_check and
-    cit.item_status_prior_to_check_in = 'Checked out' and
-    start_date <= cit.occurred_date_time and cit.occurred_date_time <= end_date
+select distinct 
+	i.jsonb ->> 'barcode' as item_barcode, 
+	lt.code,
+	lt.name,
+	concat_ws(' ', i.jsonb -> 'effectiveCallNumberComponents'->>'prefix', i.jsonb -> 'effectiveCallNumberComponents'->>'callNumber', i.jsonb ->> 'copyNumber') as item_callnumber,
+	inst.title,
+	jsonb_extract_path_text(notes.data, 'note') as Note
+from folio_inventory.item__ as i
+cross join lateral jsonb_array_elements(jsonb_extract_path(i.jsonb, 'notes')) with ordinality as notes (data)
+left join folio_inventory.item__t__ as it on i.id = it.id
+left join folio_inventory.holdings_record__t__ as hrt on it.holdings_record_id = hrt.id
+left join folio_inventory.instance__t__ as inst on hrt.instance_id = inst.id
+left join folio_inventory.location__t__ as lt on hrt.effective_location_id = lt.id
+left join folio_derived.instance_contributors as ic on inst.id = ic.instance_id
 
-	SELECT item.id AS item_id,
-       jsonb_extract_path_text(notes.jsonb, 'id')::uuid AS note_id,
-       jsonb_extract_path_text(notes.jsonb, 'note') AS note,
-       jsonb_extract_path_text(notes.jsonb, 'noteType') AS note_type,
-       jsonb_extract_path_text(notes.jsonb, 'staffOnly')::boolean AS staff_only,
-       notes.ordinality
-FROM folio_inventory.item
-    CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(jsonb, 'circulationNotes'))
-        WITH ORDINALITY AS notes (jsonb);
-
+where jsonb_extract_path_text(notes.data, 'note') like '%HIST11A%'
 
 $$
 language sql
