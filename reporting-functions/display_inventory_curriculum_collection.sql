fa63c2252c6b778703a129a661e2655f0000bb66
djones-scu-edu <154288179+djones-scu-edu@users.noreply.github.com>
2025-11-21 13:56:15 -0800

    Squashed commit of the following:
    commit 7dbbe850e7966fa205612c4bd593c498a33fbb70
    Author: lrickards <lrickards@scu.edu>
    Date:   Fri Oct 17 13:50:32 2025 -0700
    
        Update display_inventory_curriculum_collection.sql
    
    commit 84358eb5ab7809435210e6231e491310ec533b61
    Author: lrickards <lrickards@scu.edu>
    Date:   Fri Oct 17 13:49:20 2025 -0700
    
        Rename display_inventory_circulation_collection.json to display_inventory_curriculum_collection.json
    
    commit b107b071a8d18c4c0796e43b086f70527c1b5b10
    Author: lrickards <lrickards@scu.edu>
    Date:   Fri Oct 17 13:47:33 2025 -0700
    
        Create display_inventory_circulation_collection.json
    
    commit f9ab01f95a7fbe6ba2816f19ba4462c9c3a2ea41
    Author: lrickards <lrickards@scu.edu>
    Date:   Fri Oct 17 13:45:16 2025 -0700
    
        Update display_inventory_curriculum_collection.sql
    
    commit f3e575de131cff3cb83dd44b1530fe55411c100e
    Author: lrickards <lrickards@scu.edu>
    Date:   Fri Oct 17 13:43:10 2025 -0700
    
        Update display_inventory_curriculum_collection.sql
    
    commit 321dc3a1d90f4d7dc9598c9f2c4625fa677118df
    Author: lrickards <lrickards@scu.edu>
    Date:   Fri Oct 17 13:42:02 2025 -0700
    
        Update display_inventory_curriculum_collection.sql
    
    commit a9264fcf7e4c191ce4a9d8e57073b11b3497cfbb
    Author: lrickards <lrickards@scu.edu>
    Date:   Fri Oct 17 13:39:00 2025 -0700
    
        Update display_inventory_curriculum_collection.sql
    
    commit fdcd0802b83d8eeb7d10c33d086a3c59c36591d4
    Author: lrickards <lrickards@scu.edu>
    Date:   Fri Oct 17 13:38:22 2025 -0700
    
        Create display_inventory_curriculum_collection.sql

---
+++ reporting-functions/display_inventory_curriculum_collection.sql
@@ -0,0 +1,49 @@
+--metadb:function display_inventory_curriculum_collection
+
+DROP FUNCTION IF EXISTS display_inventory_curriculum_collection;
+	
+CREATE FUNCTION display_inventory_curriculum_collection(
+	i_call_number_prefix text
+)
+returns table(
+	location text,
+	index_title text,
+	contributor_name text,
+	call_number text,
+	volume text,
+	copy_number text,
+	link text,
+	item_barcode text,
+	item_id text,
+	holdings_id text
+)
+as $$
+
+SELECT distinct  
+	lt.name as "Location", 
+	int.index_title as "Title", 
+	ic.contributor_name as "Author", 
+	concat('Curr ', hrt.call_number) as "Call Number", 
+	it.volume as "Volume", 
+	it.copy_number as "Copy Number", 
+	case 
+		when int.hrid like 'b%' then concat('https://libcat.scu.edu/Record/', left(int.hrid, 8))
+		else concat('https://libcat.scu.edu/Record/', int.hrid)
+	end as "Link",
+	it.barcode as "Barcode", 
+	it.id as "Item ID", 
+	it.holdings_record_id as "Holdings ID"
+FROM 
+	folio_inventory.item__t as it
+	left join folio_inventory.holdings_record__t as hrt on it.holdings_record_id = hrt.id
+	left join folio_inventory.location__t as lt on it.effective_location_id = lt.id
+	left join folio_inventory.instance__t as int on hrt.instance_id = int.id
+	left join folio_derived.instance_contributors as ic on int.id = ic.instance_id
+where 
+	hrt.call_number_prefix = i_call_number_prefix
+	and ic.contributor_is_primary = TRUE;
+
+$$
+language sql
+STABLE 
+PARALLEL SAFE; 
